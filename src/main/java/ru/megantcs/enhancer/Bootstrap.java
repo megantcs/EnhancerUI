274b8d0414bb4c9397df77949fe10450c6695dde
megantcs <megantcs2716@gmail.com>
2025-12-15 07:27:49 +0300

    create project

---
+++ src/main/java/ru/megantcs/enhancer/Bootstrap.java
@@ -0,0 +1,12 @@
+package ru.megantcs.enhancer;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Bootstrap implements ModInitializer {
+
+    @Override
+    public void onInitialize()
+    {
+
+    }
+}
